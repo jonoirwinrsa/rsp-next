b6bea8d4a70acad9f6a025a46e4b99f5afaf707a
Jonathan Irwin <jonoirwinrsa@gmail.com>
2021-02-23 15:27:16 +0100

    feat: adds icons to sidebar and sorts out colours

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,36 +1,35 @@
-import { Button, Divider, Flex, Heading, View } from "@adobe/react-spectrum";
+import { Divider, Flex, Heading, Text, View } from "@adobe/react-spectrum";
 import Link from "next/link";
 import * as React from "react";
+
+const Button = ({ link, text, icon, background, color }) => (
+  <Link href={link}>
+    <Flex direction="row" UNSAFE_style={{ background, borderRadius: "12px", padding: "12px", color }}>
+      <img src={icon} style={{ marginRight: "10px", color }} />
+      <Text>{text}</Text>
+    </Flex>
+  </Link>
+);
 
 const Sidebar = () => (
   <View padding="size-400" minHeight="100vh" borderEndWidth="thin" borderColor="gray-200">
     <Flex direction="column" gap="size-200" minHeight="90vh" justifyContent="space-between" alignSelf="center">
       <Flex direction="row" alignItems="center">
-      <img src="logo.svg" height={30} width={30} />
-      <Heading level={1} UNSAFE_style={{ fontSize: "3rem" }}>DTeam</Heading>
+        <img src="logo.svg" height={30} width={30} />
+        <Heading level={1} UNSAFE_style={{ fontSize: "3rem" }}>
+          DTeam
+        </Heading>
       </Flex>
       <Flex direction="column" gap="size-100">
-        <Link href="/">
-          <Button variant="cta">Home</Button>
-        </Link>
-        <Link href="/dashboard">
-          <Button variant="primary">Dashboard</Button>
-        </Link>
-        <Link href="/integrations">
-          <Button variant="primary">Integrations</Button>
-        </Link>
-        <Link href="/marketplace">
-          <Button variant="primary">Marketplace</Button>
-        </Link>
+        <Button text="Home" background="#FFB800" color="white" icon="icons/home.svg" link="/" />
+        <Button text="Dashboard" background="" color="grey" icon="icons/dashboard.svg" link="/dashboard" />
+        <Button text="Integrations" background="" color="grey" icon="icons/integrations.svg" link="/integrations" />
+        <Button text="Marketplace" background="" color="grey" icon="icons/marketplace.svg" link="/marketplace" />
       </Flex>
       <Divider />
       <Flex direction="column" gap="size-100">
-        <Link href="/notifications">
-          <Button variant="primary">Notifications</Button>
-        </Link>
-        <Link href="/comments">
-          <Button variant="primary">Comments</Button>
-        </Link>
+        <Button text="Notifications" background="" color="grey" icon="icons/notifications.svg" link="/notifications" />
+        <Button text="Comments" background="" color="grey" icon="icons/chat.svg" link="/comments" />
       </Flex>
     </Flex>
   </View>
