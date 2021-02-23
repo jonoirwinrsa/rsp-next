2197b28cb398a631f5da18e8ff765ea76eff5815
Jonathan Irwin <jonoirwinrsa@gmail.com>
2021-02-23 20:44:06 +0100

    feat: adds favicon and page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { defaultTheme, Flex, Footer, Grid, Provider, View } from "@adobe/react-spectrum";
 import { SSRProvider } from "@react-aria/ssr";
+import Head from "next/head";
 import React from "react";
 import Header from "../components/Header";
 import Insights from "../components/Insights";
@@ -9,6 +10,16 @@ import Sidebar from "../components/Sidebar";
 const Home = () => (
   <SSRProvider>
     <Provider theme={defaultTheme} colorScheme="light" minHeight="100%">
+      <Head>
+        <title>DTeam</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <Grid
         areas={["sidebar  header", "sidebar content", "sidebar  footer"]}
         columns={["1fr", "4fr"]}
@@ -25,10 +36,7 @@ const Home = () => (
         <View flex={1} gridArea="sidebar">
           <Sidebar />
         </View>
-        <View
-          gridArea="content"
-          padding="size-400"
-        >
+        <View gridArea="content" padding="size-400">
           <Overview />
           <Insights />
         </View>
