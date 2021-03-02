bd8287ed974efa5a70bdda593a31a8947f6d002c
Jonathan Irwin <jonoirwinrsa@gmail.com>
2021-03-02 13:46:06 +0100

    feat: adds search and notification button to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,32 @@
-import { Header as Wrapper, Heading } from "@adobe/react-spectrum";
+import { Flex, Header as Wrapper, Heading, Text } from "@adobe/react-spectrum";
 import * as React from "react";
 
 const Header = () => (
   <Wrapper>
-    <Heading level={2} marginBottom={0}>Hi Michael Louis,</Heading>
-    <Heading level={1} marginTop={0} UNSAFE_style={{ fontSize: "3rem" }}>
-      Welcome back 👋
-    </Heading>
+    <Flex direction="row" justifyContent="space-between" alignItems="center">
+      <Flex direction="column">
+        <Heading level={2} marginBottom={0}>
+          Hi Michael Louis,
+        </Heading>
+        <Heading level={1} marginTop={0} UNSAFE_style={{ fontSize: "3rem" }}>
+          Welcome back 👋
+        </Heading>
+      </Flex>
+      <Flex>
+        <img src="icons/search.svg" />
+        <Text marginStart="size-100">Search</Text>
+      </Flex>
+      <Flex
+        UNSAFE_style={{
+          borderRadius: "50%",
+          background: "white",
+          boxShadow: "#0000001f 3px 3px 16px",
+          padding: "10px",
+        }}
+      >
+        <img src="icons/notifications.svg" />
+      </Flex>
+    </Flex>
   </Wrapper>
 );
 
