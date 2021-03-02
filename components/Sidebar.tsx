00862f445ea39670aa3fba70e904c70f0880238e
Jonathan Irwin <jonoirwinrsa@gmail.com>
2021-03-02 15:18:14 +0100

    feat: highlight items on page change

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,44 +1,55 @@
 import { Divider, Flex, Heading, Text, View } from "@adobe/react-spectrum";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import * as React from "react";
 
-const Button = ({ link, text, icon, background, color }) => (
-  <Link href={link}>
-    <a>
-      <Flex
-        direction="row"
-        alignItems="center"
-        UNSAFE_style={{ background, borderRadius: "12px", padding: "12px", color }}
-      >
-        <img src={icon} style={{ marginRight: "10px", color }} />
-        <Text>{text}</Text>
-      </Flex>
-    </a>
-  </Link>
-);
+const NavButton = ({ link, text, icon }) => {
+  const { pathname } = useRouter();
+  return (
+    <Link href={link}>
+      <a>
+        <Flex
+          direction="row"
+          alignItems="center"
+          UNSAFE_style={{
+            background: pathname === link && "#FFB800",
+            borderRadius: "12px",
+            padding: "12px",
+            color: pathname === link ? "white" : "grey",
+          }}
+        >
+          <img src={icon} style={{ marginRight: "10px", fill: pathname === link ? "white" : "grey" }} />
+          <Text>{text}</Text>
+        </Flex>
+      </a>
+    </Link>
+  );
+};
 
-const Sidebar = () => (
-  <View padding="size-400" minHeight="100vh" borderEndWidth="thin" borderColor="gray-200">
-    <Flex direction="column" gap="size-200" minHeight="90vh" justifyContent="space-between" alignSelf="center">
-      <Flex direction="row" alignItems="center">
-        <img src="logo.svg" height={30} width={30} />
-        <Heading level={1} UNSAFE_style={{ fontSize: "3rem" }}>
-          DTeam
-        </Heading>
+const Sidebar = () => {
+  return (
+    <View padding="size-400" minHeight="100vh" borderEndWidth="thin" borderColor="gray-200">
+      <Flex direction="column" gap="size-200" minHeight="90vh" justifyContent="space-between" alignSelf="center">
+        <Flex direction="row" alignItems="center">
+          <img src="logo.svg" height={30} width={30} />
+          <Heading level={1} UNSAFE_style={{ fontSize: "3rem" }}>
+            DTeam
+          </Heading>
+        </Flex>
+        <Flex direction="column" gap="size-100">
+          <NavButton text="Home" icon="icons/home.svg" link="/" />
+          <NavButton text="Dashboard" icon="icons/dashboard.svg" link="/dashboard" />
+          <NavButton text="Integrations" icon="icons/integrations.svg" link="/integrations" />
+          <NavButton text="Marketplace" icon="icons/marketplace.svg" link="/marketplace" />
+        </Flex>
+        <Divider size="M" />
+        <Flex direction="column" gap="size-100">
+          <NavButton text="Notifications" icon="icons/notifications.svg" link="/notifications" />
+          <NavButton text="Comments" icon="icons/chat.svg" link="/comments" />
+        </Flex>
       </Flex>
-      <Flex direction="column" gap="size-100">
-        <Button text="Home" background="#FFB800" color="white" icon="icons/home.svg" link="/" />
-        <Button text="Dashboard" background="" color="grey" icon="icons/dashboard.svg" link="/dashboard" />
-        <Button text="Integrations" background="" color="grey" icon="icons/integrations.svg" link="/integrations" />
-        <Button text="Marketplace" background="" color="grey" icon="icons/marketplace.svg" link="/marketplace" />
-      </Flex>
-      <Divider size="M" />
-      <Flex direction="column" gap="size-100">
-        <Button text="Notifications" background="" color="grey" icon="icons/notifications.svg" link="/notifications" />
-        <Button text="Comments" background="" color="grey" icon="icons/chat.svg" link="/comments" />
-      </Flex>
-    </Flex>
-  </View>
-);
+    </View>
+  );
+};
 
 export default Sidebar;
